a91dd67f0790028e89e9e116598f1d50994c9ded
Jeff Squyres <jsquyres@cisco.com>
2014-04-08 16:02:24 -0400

    Next bunch of software files from SVN r2003

---
+++ software/ompi/v1.8/timeline-graph.php
@@ -0,0 +1,20 @@
+<?php
+$topdir = "../../..";
+include_once("$topdir/includes/timeline-graph.inc");
+
+$vpos = 0;
+$graph = 0;
+$data = array();
+
+# The v1.8 series isn't done yet
+$beyond_end_date = strftime("%Y-%m-%d", time() + (90 * 24 * 60 * 60));
+
+//////////////////////////////////////////////////////////////////////////
+// v1.8 release series
+series("v1.8 series", "2014-03-01", $beyond_end_date, $data, $vpos, $graph);
+milestone("branch", "2014-03-25", $data, $vpos);
+milestone("v1.8", "2014-03-31", $data, $vpos);
+
+// Party on
+$graph->CreateSimple($data);
+$graph->Stroke(); 
