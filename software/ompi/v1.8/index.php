7dbd2837a2e1ce668d70962845cab2c75ae94c6b
Jeff Squyres <jsquyres@cisco.com>
2014-04-08 16:02:24 -0400

    Next bunch of software files from SVN r2003
    Former-commit-id: a91dd67f0790028e89e9e116598f1d50994c9ded

---
+++ software/ompi/v1.8/index.php
@@ -0,0 +1,156 @@
+<?php
+$topdir = "../../..";
+
+include_once("$topdir/software/ompi/v1.8/version.inc");
+$title = "Open MPI: Version $ver_v1_8";
+
+include_once("$topdir/software/ompi/nav.inc");
+include_once("$topdir/includes/header.inc");
+include_once("$topdir/includes/downloads.inc");
+
+$md5 = read_checksums("downloads/md5sums.txt");
+$sha1 = read_checksums("downloads/sha1sums.txt");
+
+$notes = "<a href=\"srpm.php\">SRPM notes</a>";
+
+#############################################################################
+#
+# Current stable release
+#
+#############################################################################
+
+$t_stable = new downloadTable("./downloads", "./downloads", dirname($_SERVER["PHP_SELF"]));
+
+if (isset($t_stable)) {
+    $src = "Version $ver_v1_8<br>$notes";
+
+    $n[] = "openmpi-$ver_v1_8.tar.bz2";
+    $n[] = "openmpi-$ver_v1_8.tar.gz";
+    $n[] = "openmpi-$ver_v1_8-1.src.rpm";
+
+    foreach ($n as $name) {
+        if (file_exists("downloads/$name")) {
+            $t_stable->addFile($src, $name, $md5[$name], $sha1[$name]);
+        }
+    }
+
+    $t_stable->addLink("Cygwin support", 
+		       "Precompiled Open MPI packages for Cygwin",
+		       date("Y"),
+		       "http://cygwin.com/cgi-bin2/package-grep.cgi?grep=openmpi"
+		       );
+}
+
+#############################################################################
+#
+# Prereleases
+#
+#############################################################################
+
+# See if there's a snapshot file that does not have any letters in its
+# version number (i.e., is not a stable release) and has a
+# corresponding .tar.bz2 file or .tar.gz here in the downloads
+# directory.
+
+$fp = fopen("downloads/latest_snapshot.txt", "r");
+$v = fgets($fp);
+fclose($fp);
+if (strlen($v) > 0) {
+    $v = substr($v, 0, strlen($v) - 1);
+}
+
+if (preg_match("/[a-z]/i", $v) &&
+    (file_exists("downloads/openmpi-$v.tar.bz2") ||
+     file_exists("downloads/openmpi-$v.tar.gz") ||
+     file_exists("downloads/openmpi-$v-1.src.rpm"))) {
+    $t_prerelease = new downloadTable("./downloads", "./downloads", 
+                                      dirname($_SERVER["PHP_SELF"]));
+
+    $s = "$v (pre-release)<br>$notes";
+
+    $n = array();
+    $n[] = "openmpi-$v.tar.bz2";
+    $n[] = "openmpi-$v.tar.gz";
+    $n[] = "openmpi-$v-1.src.rpm";
+
+    foreach ($n as $name) {
+        if (file_exists("downloads/$name")) {
+            $t_prerelease->addFile($s, $name, $md5[$name], $sha1[$name]);
+        }
+    }
+}
+
+#############################################################################
+#
+# Older releases
+#
+#############################################################################
+
+# Uncomment this when v1.8.1 is released
+#$t_older = new downloadTable("./downloads", "./downloads", dirname($_SERVER["PHP_SELF"]));
+
+if (isset($t_older)) {
+    $versions = array();
+    $versions[] = "1.8";
+
+    foreach ($versions as $v) {
+        $n = array();
+        $n[] = "openmpi-$v.tar.bz2";
+        $n[] = "openmpi-$v.tar.gz";
+        $n[] = "openmpi-$v-1.src.rpm";
+
+        $s = "Version $v<br>$notes";
+
+        foreach ($n as $name) {
+            if (file_exists("downloads/$name")) {
+                $t_older->addFile($s, $name, $md5[$name], $sha1[$name]);
+            }
+        }
+    }
+}
+
+#############################################################################
+#
+# Main display part of the page
+#
+#############################################################################
+
+$project = "Open MPI";
+$list_name = "announce";
+$prev_describe = "the v$ver_v1_6 download page";
+include_once("$topdir/includes/subscribe-announce.inc");
+?>
+
+<p><hr>
+
+<P><? $dir = "svn.open-mpi.org/svn/ompi/branches/v1.8/NEWS";
+      print("<a href=\"http://$dir\">"); ?>This
+file</a> contains a list of changes between the releases in the Open
+MPI releases in the v1.8 series</p>
+
+<p>See the <a href="<?php print($topdir);
+?>/software/ompi/versions/timeline.php">version timeline</a> for
+information on the chronology of Open MPI releases.</p>
+
+<p>
+<div align="center">
+
+<?
+if (isset($t_stable)) {
+    print("</div>\n\n<p>Current stable release:</p>\n\n<p>\n<div align=center>\n\n");
+    $t_stable->printMe();
+}
+if (isset($t_prerelease)) {
+    print("</div>\n\n<p>Prerelease:</p>\n\n<p>\n<div align=center>\n\n");
+    $t_prerelease->printMe();
+}
+if (isset($t_older)) {
+    print("</div>\n\n<p>Previous releases:</p>\n\n<p>\n<div align=center>\n\n");
+    $t_older->printMe();
+}
+?>
+</div>
+</p>
+
+<?php 
+  include_once("$topdir/includes/footer.inc");
---
+++ software/ompi/v1.8/srpm.php
@@ -0,0 +1,43 @@
+<?php
+  $topdir = "../../..";
+  include_once("$topdir/software/ompi/v1.8/version.inc");
+
+  $title = "Open MPI: Version $ver_v1_8 Source RPM Notes";
+  include_once("$topdir/software/ompi/nav.inc");
+  include_once("$topdir/includes/header.inc");
+  include_once("$topdir/includes/curl_get.inc");
+
+  include_once("$topdir/includes/downloads.inc");
+
+  $base_url = "http://svn.open-mpi.org/svn/ompi/branches/v1.8";
+  $linux_dir = "$base_url/contrib/dist/linux";
+  $readme = "$linux_dir/README";
+  $specfile = "$linux_dir/openmpi.spec";
+?>
+
+<p>Because every parallel computation environment is different, Open MPI
+is a highly configurable piece of software.  As such, a single set of
+build options is not sufficient to meet everyone's needs.  The source
+RPM therefore accepts a wide variety of configuration options on the
+"<code>rpmbuild --rebuild</code>" command line.  The following text
+describes the options that are available.</p>
+
+<p>A script to build an Open MPI RPM from a source tarball <a
+href="<?php print($base_url); ?>/contrib/dist/linux/buildrpm.sh">is
+available here</a>, and is referred to as "<code>buildrpm.sh</code>"
+in the text below.  Additionally, you can build an Open MPI RPM from
+the SRPM (also described below).</p>
+
+<p>The text below is specific to the v1.8 series of Open MPI and <a
+href="<?php print($readme); ?>">is available here in a
+printer-friendly format</a>.
+
+<p>Note that the spec file (that the text below refers to) has many
+more comments and explanations of rpmbuild-time configuration options,
+and <a href="<?php print($specfile); ?>">is available here</a>.
+
+<?php 
+  $str = do_curl_get($readme);
+  print("<p><hr>\n<pre>\n$str\n</pre><p><hr>\n\n");
+  
+  include_once("$topdir/includes/footer.inc");
