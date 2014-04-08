a09ccf3d4dfc33e68baafb4d0a61dd474221978c
Jeff Squyres <jsquyres@cisco.com>
2014-04-08 15:57:37 -0400

    Next bunch of software files from SVN r2003
    Former-commit-id: 863d4ead803004412bd42633e78d5bcf695629e5

---
+++ software/hwloc/nightly/index.php
@@ -0,0 +1,49 @@
+<?php
+  $topdir = "../../..";
+  $title = "Hardware Locality (hwloc): Nightly snapshot tarballs";
+  include_once("$topdir/projects/hwloc/nav.inc");
+  include_once("$topdir/includes/header.inc");
+?>
+
+The following versions are available as nightly snapshots:
+
+<p>
+<ul>
+
+<p> <strong>Current stable release series</strong>
+
+<li><a href="v1.9/"><strong>v1.9 series</strong></a> (stable
+release series): These snapshots are from the 1.9 branch and reflect
+the latest progression in the 1.9.x series.  The emphasis of this tree
+is on bug fixes and stability.  <strong>This is the recommended series for
+all users to download and use.</strong></li>
+
+<!--
+<p> <strong>Upcoming stable release series</strong>
+
+<li><a href="v1.10/"><strong>v1.10 series</strong></a> (upcoming stable
+release series): These snapshots are from the 1.10 branch and reflect
+the latest progression in the 1.10.x series.  The emphasis of this tree
+is on bug fixes and stability.  <strong>This tree is not ready for
+prime time consumption yet.</strong></li>
+-->
+
+<p> <strong>Previous stable release series</strong>
+
+<li><a href="v1.8/"><strong>v1.8 series</strong></a> (previous stable
+release series): These snapshots are from the 1.7 branch and reflect
+the latest progression in the 1.8.x series.  The emphasis of this tree
+is on bug fixes and stability.</li>
+
+<p> <strong>Current development</strong>
+
+<li><a href="master/"><strong>Master</strong></a> (development head):
+These snapshots are from the git master branch and reflect the current
+head of development.  The usual disclaimers about the state of
+development code apply.</li>
+
+</ul>
+</p>
+
+<?php 
+  include_once("$topdir/includes/footer.inc"); 
