c0479ce3679480ad1b6c059ba7a449468adc6d75
Jeff Squyres <jsquyres@cisco.com>
2014-04-09 09:44:49 -0400

    Remove commented out code
    Former-commit-id: 2cbf13a61c337a3f0f0cf3ab4aa56cb2316f4241

--- search.php
+++ search.php
@@ -3,8 +3,6 @@
   $title = "Open MPI: <br />Open Source High Performance Computing";
   include_once("$topdir/includes/mailto.inc");
   include_once("$topdir/includes/header.inc");
-#  include_once("$topdir/includes/news.inc");
-
 ?>
 
 <IFRAME src="http://www.open-mpi.org/cgi-bin/swish.cgi"width=905 height=500 scrolling="yes" scrollbars="yes" ></IFRAME>
