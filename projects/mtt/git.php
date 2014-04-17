83266894e3ca6abc0dc9d7f6c6a2744477b87d6b
Jeff Squyres <jsquyres@cisco.com>
2014-04-17 10:16:53 -0400

    Switch MTT over to git/github
    Former-commit-id: 0e30668be1ead70a5ae9a4e18a9eeabb3a072426

---
+++ projects/mtt/git.php
@@ -0,0 +1,13 @@
+<?php
+$topdir = "../..";
+$title = "MPI Testing Tool (MTT): Git Clone";
+include_once("nav.inc");
+include_once("$topdir/includes/header.inc");
+include_once("$topdir/includes/code.inc");
+?>
+
+MTT's Git repository <a href="https://github.com/open-mpi/mtt">is hosted
+at Github</a>.
+
+<?php 
+  include_once("$topdir/includes/footer.inc"); 
