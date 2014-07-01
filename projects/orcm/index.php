fa662e501100f1ee8eb755f6b170419dcbca5bf6
Ralph Castain <rhc@open-mpi.org>
2014-07-01 13:16:47 -0700

    Add more info to the ORCM page
    Former-commit-id: f4990e822e532b661736e66a445dfdea3113dc28

--- projects/orcm/index.php
+++ projects/orcm/index.php
@@ -7,10 +7,8 @@ include_once("$topdir/includes/news.inc");
 include_once("$topdir/includes/code.inc");
 ?>
 
-<p> The Open Resilient Cluster Manager (ORCM) project.</p>
-
 <p>ORCM was originally developed as an open-source project (under the Open MPI license)
-   by Ralph Castain while employed at Cisco to provide
+   by Cisco Systems, Inc to provide
    a resilient, 100% uptime run-time environment for enterprise-class routers. Based on the
    Open Run-Time Environment (ORTE) embedded in Open MPI, the system provided launch and
    execution support for processes executing within the router itself
@@ -26,7 +24,7 @@ include_once("$topdir/includes/code.inc");
    analytics framework that allowed users to chain various analysis modules in the
    data pipeline so as to perform <i>in-flight</i> data reduction.<p>
 
-<p>Subsequently, ORCM was extended by Ralph while at Greenplum to serve as a scalable
+<p>Subsequently, ORCM was extended by Greenplum to serve as a scalable
    monitoring system for Hadoop clusters. While ORCM itself had run on quite a few
    "nodes" in the Cisco router, and its base ORTE platform has been used for years
     on very large clusters involving many thousands of nodes, this was the first time
@@ -37,10 +35,114 @@ include_once("$topdir/includes/code.inc");
     enough that in-flight data reduction was not required, nor was fault prediction considered
     to be of value in the Hadoop paradigm.</p>
 
+<p>However, data flows did require introduction of an <i>aggregator</i> role. Aggregators absorb the data
+   sent by other nodes and can either store the data in a database, analyze the data, or both. The objective
+   of the aggregator is primarily to concentrate the database operations, thus minimizing the number of
+   active connections to the database itself.</p>
+
 <p>Throughout this time, ORCM has retained ORTE's ability to perform scalable launch and
    process monitoring, and ORTE's support for a variety of scheduling environments. We are
    now in the process of validating and extending ORCM to provide both monitoring and
    launch support for exascale environments.</p>
+
+<!------------------------------------------------------------------------->
+
+<h3>Core Features</h3>
+
+<ul>
+<li>Plugin architecture based on Open MPI's Module Component Architecture (MCA)
+<ul>
+<li>Sophisticated auto-select algorithms based on system size, available resources</li>
+<li>Binary proprietary plugin support</li>
+<li>On-the-fly updates for maintenance*</li>
+<li>Addition of new plugin capabilities/features without requiring system-wide restart if compatibility requirements are met</li>
+</ul>
+</li>
+<li>Hardware discovery support
+<ul>
+<li>Automatic reporting of hardware inventory on startup</li>
+<li>Automatic updating upon node removal and replacement</li>
+</ul>
+<li>Provisioning support
+<ul>
+<li>Images provisioned based on user directive prior to launch*</li>
+</ul>
+<li>Scalable overlay network
+<ul>
+<li>Supports multiple topologies, including both tree and mesh plugins</li>
+<li>Automatic route failover and restoration, messages cached pending comm recovery</li>
+<li>Both in-band and out-of-band transports with auto-failover between them</li>
+</ul>
+</li>
+<li>Sensors
+<ul>
+<li>Both push and pull models supported</li>
+<li>Read as a group at regular intervals according to a specified rate, or individual sensors can be read at their own regular interval, or individual readings of any combination of sensors can be polled upon request</li>
+<ul><li>Polling requests can return information directly to the caller, or can include the reading in the next database update, as specified by the caller</li></ul>
+<li>Data collected locally and sent to an aggregator for recording into a database at specified time intervals</li>
+</li>
+<li>Environment sensors</li>
+<ul>
+<li>Processor temperature - on-board sensor for reading processor temperatures when coretemp kernel module loaded</li>
+<li>Processor frequency - on-board sensor for reading processor frequencies. Requires read access to /sys/devices/system/cpu directory</li>
+<li>IPMI readings of AC power, cabinet temperature, water and air temperatures, etc.</li>
+<li>Processor power - reading processor power from on-board MSR. Only supported for Intel SandyBridge, IvyBridge, and Haswell processors</li>
+</ul>
+<li>Resource utilization</li>
+<ul>
+<li>Wide range of process and node-level resource utilization, including memory, cpu, network I/O, and disk I/O</li>
+</ul>
+<li>Process failure</li>
+<ul>
+<li>Monitors specified file(s) for programmatic access within specified time intervals and/or file size restrictions</li>
+<li>Heartbeat monitoring</li>
+</ul>
+</ul>
+<li>Analytics*
+<ul>
+<li>Supports in-flight reduction of sensor data</li>
+<li>User-defined workflows for data analysis using available plugins connected in user-defined chains</li>
+<li>Analysis chain outputs can be included in database reporting or sent to requestor at user direction</li>
+<li>Plugins can support event and alert generation</li>
+<li>"Tap" plugin directs copied stream of selected data from specified point to remote requestor</li>
+<li>Chains can be defined at any aggregation point in system</li>
+</ul>
+<li>Database
+<ul>
+<li>Both raw and processed data can be stored in one or more databases</li>
+<li>Supports both SQL and non-SQL* databases</li>
+<ul>
+<li>Multiple instances of either type can be used in parallel*</li>
+<li>Target database for different data sources and/or types can be specified using MCA parameters during configure and startup, and can be altered by command during operation*</li>
+</ul>
+</li>
+<li>Cross-data correlation maintained
+<ul>
+<li>Relationship between job, sensor, and performance data tracked and linked for easy retrieval*</li>
+</ul>
+</li>
+</ul>
+</li>
+<li>Scalable launch
+<ul>
+<li>Distributed mapping system to minimize data transmission of launch commands*</li>
+<li>PMIx wireup support for MPI applications*</li>
+<li>Automatic pre-positioning of dynamic libraries*</li>
+<li>Pre-loading of libraries and data by user directive*</li>
+</ul>
+</li>
+<li>Fault Tolerance
+<ul>
+<li>Self-healing communication system (see above)</li>
+<li>Non-heartbeat detection of node failures</li>
+<li>Automatic state recovery based on retrieval of state information from peers*</li>
+<li>Support for time-based checkpoint of applications*</li>
+<li>Burst buffer management for rapid checkpoint/restart*</li>
+</ul>
+</li>
+</ul>
+
+<p>*indicates areas of development</p>
 
 <!------------------------------------------------------------------------->
 
