6d2cf4c67ec70aec204c1140c90c6892f261e04f
Ralph Castain <rhc@open-mpi.org>
2014-09-24 09:08:51 -0700

    Update PMIx links to point to Github areas
    Former-commit-id: a4ed1e45bc74bffd662ccb5da98864dbfcd56210

--- projects/orcm/license.php
+++ projects/orcm/license.php
@@ -7,7 +7,7 @@ include_once("$topdir/includes/curl_get.inc");
 ?>
 
 <p><strong>The Open Resilient Cluster Manager (ORCM) is distributed under the <?php
-print("<a href=\"http://www.opensource.org/licenses/bsd-license.php\">");
+print("<a href=\"http://opensource.org/licenses/BSD-3-Clause\">");
 ?>New BSD license</a>, listed below.</strong>
 
 <?php
