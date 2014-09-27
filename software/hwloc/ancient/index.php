b1d8d8744ccc86fd50b7bced5cc531a8b81a0fb5
Brice Goglin <Brice.Goglin@inria.fr>
2014-09-27 15:04:38 +0200

    hwloc: move ancient release series download from nav.inc to a dedicated page
    Former-commit-id: bb10d93ae2a93e6012878b719ce87b004fbbf486

---
+++ software/hwloc/ancient/index.php
@@ -0,0 +1,27 @@
+<?php
+$topdir = "../../..";
+
+$title = "Portable Hardware Locality (hwloc): ancient release series";
+
+include_once("$topdir/projects/hwloc/nav.inc");
+include_once("$topdir/includes/header.inc");
+include_once("$topdir/includes/downloads.inc");
+?>
+
+<p>
+ Only recent release series are listed in the left panel.
+ Here are the links to ancient release series archives:
+</p>
+<ul>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.6/">Download v1.6 (ancient)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.5/">Download v1.5 (Middle Age)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.4/">Download v1.4 (Antiquity)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.3/">Download v1.3 (Stone Age)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.2/">Download v1.2 (Cretaceous)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.1/">Download v1.1 (Jurassic)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.0/">Download v1.0 (Precambrian)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v0.9/">Download v0.9 (Big-Bang)</a></li>
+</ul>
+
+<?php
+  include_once("$topdir/includes/footer.inc");
