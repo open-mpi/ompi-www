52efbb5c343112b03ac2ef605216b59d53f11107
Jeff Squyres <jsquyres@cisco.com>
2014-10-28 09:09:40 -0400

    SC2014: add pre-BOF questionaire
    Former-commit-id: 69a98529a225c3d9ab415c68a4eef076c8e23a55

---
+++ sc2014/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$topdir = "..";
+$title = "Open MPI State of the Union BOF at SC'14";
+$short_title = "OMPI SotU BOF@SC'13";
+#$search_name = "just the FAQ";
+#$search_subdir = "/faq/";
+
+include_once("$topdir/includes/header.inc");
+
+?>
+
+<iframe src="https://docs.google.com/forms/d/1uzxcOwN4NQS2e9P6DYc4bvHA2dkDD7A03aMxMUgtjPY/viewform?embedded=true" width="760" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
+<?php
+include_once("$topdir/includes/footer.inc"); 
