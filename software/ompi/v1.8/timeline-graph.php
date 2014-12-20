7dae38c0670d3a06d110372ec4c80a17033056dd
Ralph Castain <rhc@open-mpi.org>
2014-12-19 19:27:37 -0800

    Release v1.8.4
    Former-commit-id: 55295032d4f805fed1a0a66df3ad6dc00946e107

--- software/ompi/v1.8/timeline-graph.php
+++ software/ompi/v1.8/timeline-graph.php
@@ -17,6 +17,7 @@ milestone("v1.8", "2014-03-31", $data, $vpos);
 milestone("v1.8.1", "2014-04-23", $data, $vpos);
 milestone("v1.8.2", "2014-08-25", $data, $vpos);
 milestone("v1.8.3", "2014-09-26", $data, $vpos);
+milestone("v1.8.4", "2014-12-19", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
