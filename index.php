48215591448193dfa79e6cc27b60cfa23d58ccf7
Brice Goglin <Brice.Goglin@inria.fr>
2015-01-26 14:20:47 +0100

    hwloc news
    Former-commit-id: 3bbae80381d6b6850decce9b65dd72caad52cfb4

--- index.php
+++ index.php
@@ -90,9 +90,9 @@ information</a></h3>
 news("Open MPI v1.8.4 released",
      "Stable release",
      "http://www.open-mpi.org/community/lists/announce/2014/12/0066.php");
-news("hwloc v1.10.0 released",
-     "New feature release",
-     "http://www.open-mpi.org/community/lists/hwloc-announce/2014/10/0072.php");
+news("hwloc v1.10.1 released",
+     "New stable release",
+     "http://www.open-mpi.org/community/lists/hwloc-announce/2015/01/0075.php");
 news("ORCM v0.5.0 released",
      "New preview release",
      "http://www.open-mpi.org/community/lists/orcm-announce/2014/11/0000.php");
