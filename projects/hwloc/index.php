9efb358e778ea06bb781084bab62389f8db71c5f
Brice Goglin <Brice.Goglin@inria.fr>
2015-01-27 14:58:18 +0100

    hwloc: remove old news
    Former-commit-id: 42ce2f5eba69bf5571a33d99c450d9ca41e12889

--- projects/hwloc/index.php
+++ projects/hwloc/index.php
@@ -19,9 +19,6 @@ include_once("$topdir/includes/code.inc");
 news("hwloc v1.10.1 released",
      "New stable release",
      "http://www.open-mpi.org/community/lists/hwloc-announce/2015/01/0075.php");
-news("hwloc v1.10.0 released",
-     "New feature release",
-     "http://www.open-mpi.org/community/lists/hwloc-announce/2014/10/0072.php");
 news("R&D engineer position",
      "at Inria Bordeaux (France)",
      "http://www.open-mpi.org/community/lists/hwloc-users/2014/10/1113.php");
