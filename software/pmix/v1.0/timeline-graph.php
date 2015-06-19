a4cb9fda0b70e7c4efc1dad0314de9cb0055401a
Ralph Castain <rhc@open-mpi.org>
2015-06-19 10:01:40 -0700

    Release PMIx 1.0.0

--- software/pmix/v1.0/timeline-graph.php
+++ software/pmix/v1.0/timeline-graph.php
@@ -13,7 +13,7 @@ $beyond_end_date = strftime("%Y-%m-%d", time() + (90 * 24 * 60 * 60));
 // v1.0 release series
 series("v1.0 series", "2015-05-01", $beyond_end_date, $data, $vpos, $graph);
 milestone("branch", "2015-05-25", $data, $vpos);
-milestone("v1.0.0", "2015-05-31", $data, $vpos);
+milestone("v1.0.0", "2015-06-19", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
