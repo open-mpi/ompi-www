5c047bb285053d61e264d892c3a9e881b53a72a1
Jeff Squyres <jsquyres@cisco.com>
2015-06-20 12:53:00 -0400

    Updates for v2.x support.
    Former-commit-id: 65089202e378e1078c2aa2b4e765b7c5cf079c47

--- software/ompi/v1.10/timeline-graph.php
+++ software/ompi/v1.10/timeline-graph.php
@@ -6,12 +6,23 @@ $vpos = 0;
 $graph = 0;
 $data = array();
 
-# The v1.8 series isn't done yet
-$beyond_end_date = strftime("%Y-%m-%d", time() + (90 * 24 * 60 * 60));
+$first_date = "2015-05-01";
+
+# The graph needs about a year time span for the title to display properly.
+# So pick either a year beyond the first date or 90 days beyond now,
+# whichever is greater.
+
+# 3 months from today
+$ts1 = time() + (90 * 24 * 60 * 60);
+# 1 year beyond the first date (this i
+$ts2 = mktime(0, 0, 0, 5, 1, 2016);
+
+$ts = max($ts1, $ts2);
+$beyond_end_date = strftime("%Y-%m-%d", $ts);
 
 //////////////////////////////////////////////////////////////////////////
-// v1.8 release series
-series("v1.10 series", "2015-05-01", $beyond_end_date, $data, $vpos, $graph);
+// v1.10 release series
+series("v1.10 series", $first_date, $beyond_end_date, $data, $vpos, $graph);
 milestone("branch", "2015-05-23", $data, $vpos);
 
 // Party on
