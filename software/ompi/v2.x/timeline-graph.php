65089202e378e1078c2aa2b4e765b7c5cf079c47
Jeff Squyres <jsquyres@cisco.com>
2015-06-20 12:53:00 -0400

    Updates for v2.x support.

--- software/ompi/v2.x/timeline-graph.php
+++ software/ompi/v2.x/timeline-graph.php
@@ -6,13 +6,24 @@ $vpos = 0;
 $graph = 0;
 $data = array();
 
-# The v2.x series isn't done yet
-$beyond_end_date = strftime("%Y-%m-%d", time() + (90 * 24 * 60 * 60));
+$first_date = "2015-06-20";
+
+# The graph needs about a year time span for the title to display properly.
+# So pick either a year beyond the first date or 90 days beyond now,
+# whichever is greater.
+
+# 3 months from today
+$ts1 = time() + (90 * 24 * 60 * 60);
+# 1 year beyond the first date (this i
+$ts2 = mktime(0, 0, 0, 6, 1, 2016);
+
+$ts = max($ts1, $ts2);
+$beyond_end_date = strftime("%Y-%m-%d", $ts);
 
 //////////////////////////////////////////////////////////////////////////
 // v2.x release series
 series("v2.x series", "2015-06-01", $beyond_end_date, $data, $vpos, $graph);
-milestone("branch", "2015-06-16", $data, $vpos);
+milestone("branch", "2015-06-20", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
