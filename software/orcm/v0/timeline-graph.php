f66f89ac9013d1da48619c2fb3fd90ef2ddd7968
Ralph Castain <rhc@open-mpi.org>
2015-08-19 16:45:27 -0700

    Release ORCM v0.11.0
    Former-commit-id: 77ae2eb24d7f62af8d666b5dc4e3c542f454090d

--- software/orcm/v0/timeline-graph.php
+++ software/orcm/v0/timeline-graph.php
@@ -11,11 +11,12 @@ $beyond_end_date = strftime("%Y-%m-%d", time() + (90 * 24 * 60 * 60));
 
 //////////////////////////////////////////////////////////////////////////
 // v0 release series
-series("v0 series", "2014-10-01", $beyond_end_date, $data, $vpos, $graph);
-milestone("branch", "2014-10-01", $data, $vpos);
-milestone("v0.5.0", "2014-11-05", $data, $vpos);
-milestone("v0.7.0", "2015-02-25", $data, $vpos);
-milestone("v0.9.0", "2015-06-29", $data, $vpos);
+series("v0 series",  "2014-10-01", $beyond_end_date, $data, $vpos, $graph);
+milestone("branch",  "2014-10-01", $data, $vpos);
+milestone("v0.5.0",  "2014-11-05", $data, $vpos);
+milestone("v0.7.0",  "2015-02-25", $data, $vpos);
+milestone("v0.9.0",  "2015-06-29", $data, $vpos);
+milestone("v0.11.0", "2015-08-19", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
