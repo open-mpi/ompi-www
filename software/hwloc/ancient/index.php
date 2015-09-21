92b45a831757c722f5c557b75cdece776e722cc1
Brice Goglin <Brice.Goglin@inria.fr>
2015-09-21 18:36:14 +0200

    hwloc: move v1.8 downloads to ancient section
    Former-commit-id: 60854ea21b1d8e862986ef90d38aaa649c57f385

--- software/hwloc/ancient/index.php
+++ software/hwloc/ancient/index.php
@@ -13,14 +13,15 @@ include_once("$topdir/includes/downloads.inc");
  Here are the links to ancient release series archives:
 </p>
 <ul>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.7/">Download v1.7 (old)</a></li>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.6/">Download v1.6 (ancient)</a></li>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.5/">Download v1.5 (Middle Age)</a></li>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.4/">Download v1.4 (Antiquity)</a></li>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.3/">Download v1.3 (Stone Age)</a></li>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.2/">Download v1.2 (Cretaceous)</a></li>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.1/">Download v1.1 (Jurassic)</a></li>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.0/">Download v1.0 (Precambrian)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.8/">Download v1.8 (Antiquity)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.7/">Download v1.7 (Iron Age)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.6/">Download v1.6 (Bronze Age)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.5/">Download v1.5 (Stone Age)</a></li> <!-- contains Mesolithic and Neolithic -->
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.4/">Download v1.4 (Cretaceous)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.3/">Download v1.3 (Jurassic)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.2/">Download v1.2 (Precambrian)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.1/">Download v1.1 (Solar System formation)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.0/">Download v1.0 (Early Universe)</a></li> <!-- https://en.wikipedia.org/wiki/Chronology_of_the_universe -->
 <li><a href="<?php echo $topdir; ?>/software/hwloc/v0.9/">Download v0.9 (Big-Bang)</a></li>
 </ul>
 
