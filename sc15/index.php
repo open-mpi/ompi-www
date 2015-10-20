f297891580f89ba908d78ce18eb34fc43efae074
Jeff Squyres <jsquyres@cisco.com>
2015-10-20 15:14:06 -0400

    sc15: add a little more info about both BOFs

--- sc15/index.php
+++ sc15/index.php
@@ -6,16 +6,27 @@ $title = "Supercomputing 2015 Birds-of-a-Feather Meetings";
 include_once("$topdir/includes/header.inc");
 ?>
 
-<h4>Community members have two BoF meetings at SC'15!</h4>
+<h4>Community members have two BoF meetings at <a href="http://sc15.supercomputing.org/">SC'15</a>!</h4>
 
 <P>We <i>really</i> value your questions, and want
 to ensure we can respond to as many of them as possible in the allotted 1 hour time. So please
 take a few minutes and submit your questions in advance.</p>
 
 <p>
+<ol>
+<li><strong>Open MPI State of the Union</strong>
 <ul>
-<li><a href="ompi/">Open MPI</a></li>
-<li><a href="pmix/">Program Management Interface - Exascale</a></li>
+<li><a href="http://sc15.supercomputing.org/schedule/event_detail?evid=bof107">12:15-1:15pm, Wednesday, November 18th, 2015, room 18CD</a>.</li>
+<li><a href="ompi/">Submit your question for the Open MPI BOF here</a></li>
+</ul></li>
+
+<br />
+
+<li><strong>Program Management Interface - Exascale (PMIx)</strong>
+<ul>
+<li><a href="http://sc15.supercomputing.org/schedule/event_detail?evid=bof122">12:15-1:15pm, Thursday, November 19th, 2015, room 15</a>.</li>
+<li><a href="pmix/">Submit your question for the PMIx BOF here</a></li>
+</ul></li>
 </ul>
 </p>
 
