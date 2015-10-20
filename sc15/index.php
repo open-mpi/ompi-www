a41d22d25c1da494039d6c0d1c2b2fe7b2d0e198
Ralph Castain <rhc@open-mpi.org>
2015-10-20 12:04:55 -0700

    Add the PMIx BoF page, and reorganization the page per discussion with Jeff

--- sc15/index.php
+++ sc15/index.php
@@ -1,17 +1,23 @@
 <?php
-
 $topdir = "..";
-$title = "Open MPI State of the Union BOF at SC'15";
-$short_title = "OMPI SotU BOF@SC'15";
+
+$title = "Supercomputing 2015 Birds-of-a-Feather Meetings";
 
 include_once("$topdir/includes/header.inc");
-
 ?>
 
-(please wait for the Google form to load...)
+<h4>Community members have two BoF meetings at SC'15!</h4>
+
+<P>We <i>really</i> value your questions, and want
+to ensure we can respond to as many of them as possible in the allotted 1 hour time. So please
+take a few minutes and submit your questions in advance.</p>
 
 <p>
-<iframe src="https://docs.google.com/forms/d/1KatBcTNBPpkXT_BT8xDFG6qsUgcMWmYTwx3WaKqKVuE/viewform?embed=true" width="760" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+<ul>
+<li><a href="ompi/">Open MPI</a></li>
+<li><a href="pmix/">Program Management Interface - Exascale</a></li>
+</ul>
+</p>
 
 <?php
-include_once("$topdir/includes/footer.inc"); 
+  include_once("$topdir/includes/footer.inc");
