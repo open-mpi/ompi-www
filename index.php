36b083f316c10f0143712e0214787bb34a16d919
Jeff Squyres <jsquyres@cisco.com>
2016-07-09 08:05:47 -0400

    Remove stale SC15 reference
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- index.php
+++ index.php
@@ -87,9 +87,6 @@ information</a></h3>
 
 <?php
 # Lists are not mirrored - use an absolute URL
-news("SC'15 OMPI State of the Union and PMIx BOFs",
-     "Submit your questions ahead of time for the SC'15 BOFs",
-     "sc15/");
 news("Open MPI v1.10.3 released",
      "Stable release",
      "https://www.open-mpi.org/community/lists/announce/2016/06/0083.php");
