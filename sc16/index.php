7bec10480ced9148324f6dbea2a7720aa06aae53
Ralph Castain <rhc@open-mpi.org>
2016-11-16 09:41:36 -0600

    Addition of SC16 resources
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

---
+++ sc16/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$topdir = "..";
+$title = "Open MPI State of the Union X BOF at SC'16";
+$short_title = "OMPI SotU BOF@SC'16";
+
+include_once("$topdir/includes/header.inc");
+
+?>
+
+Resources for during / after the SC'16 Open MPI State of the Union BOF:
+
+<ol>
+<li> <a href="http://sc16.supercomputing.org/presentation/?id=bof103&sess=sess322">Submit a question during the BOF</a> </li>
+
+<br />
+
+<li> <a href="https://docs.google.com/forms/d/e/1FAIpQLSfesd1wde02moEdYia4NUrLVdw6wcPJdupMTzKwyv0aPEerXg/viewform">Submit your opinion on the next version series of Open MPI</a></li>
+
+</ol>
+
+<?php
+include_once("$topdir/includes/footer.inc");
