24fff17d33e69e23a212d48a1a9464439efdeabc
Ralph Castain <rhc@open-mpi.org>
2016-11-17 07:48:44 -0600

    Remove the in-BOF linklings link
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>
    
    Former-commit-id: 0aa94b5cc8e8d054fb3a1698f081bd9d482c7c98

--- sc16/index.php
+++ sc16/index.php
@@ -11,9 +11,6 @@ include_once("$topdir/includes/header.inc");
 Resources for during / after the SC'16 Open MPI State of the Union BOF:
 
 <ol>
-<li> <a href="http://sc16.supercomputing.org/presentation/?id=bof103&sess=sess322">Submit a question during the BOF</a> </li>
-
-<br />
 
 <li> <a href="https://docs.google.com/forms/d/e/1FAIpQLSfesd1wde02moEdYia4NUrLVdw6wcPJdupMTzKwyv0aPEerXg/viewform">Submit your opinion on the next version series of Open MPI</a></li>
 
