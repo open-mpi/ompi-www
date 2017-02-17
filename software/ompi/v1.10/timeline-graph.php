6654adabbed8f599fc4fc50d9568ee90cbedf543
Ralph Castain <rhc@open-mpi.org>
2017-02-17 13:21:22 -0800

    Release OMPI v1.10.6
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- software/ompi/v1.10/timeline-graph.php
+++ software/ompi/v1.10/timeline-graph.php
@@ -30,6 +30,7 @@ milestone("v1.10.2", "2016-01-26", $data, $vpos);
 milestone("v1.10.3", "2016-06-15", $data, $vpos);
 milestone("v1.10.4", "2016-09-01", $data, $vpos);
 milestone("v1.10.5", "2016-12-19", $data, $vpos);
+milestone("v1.10.6", "2017-02-17", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
