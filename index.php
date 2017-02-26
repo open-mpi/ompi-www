f2e13232e41d070a83314366f93a8f6664ab4860
Jeff Squyres <jsquyres@cisco.com>
2017-02-26 08:37:01 -0600

    Add v2.1.0rc1 announcement link
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>
    
    Former-commit-id: 880dbd0bb68aa352746b4d5c6d99504d8c0f0f9b

--- index.php
+++ index.php
@@ -87,6 +87,9 @@ information</a></h3>
 
 <?php
 # Lists are not mirrored - use an absolute URL
+news("Open MPI v2.1.0rc1 released",
+     "New stable series pre-release",
+     "https://www.mail-archive.com/announce@lists.open-mpi.org//msg00097.html");
 news("Open MPI v2.0.2 released",
      "Bug fix release",
      "https://www.mail-archive.com/announce@lists.open-mpi.org//msg00095.html");
