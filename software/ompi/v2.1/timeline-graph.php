c9f6ed7fcb9ae7b72092fe5e833ef4f964f0febf
Jeff Squyres <jsquyres@cisco.com>
2017-03-21 11:52:18 -0500

    Release v2.1.0
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- software/ompi/v2.1/timeline-graph.php
+++ software/ompi/v2.1/timeline-graph.php
@@ -25,7 +25,7 @@ $beyond_end_date = strftime("%Y-%m-%d", $ts);
 series("v2.1 series", "2015-06-01", $beyond_end_date, $data, $vpos, $graph);
 milestone("branch from master", "2015-06-20", $data, $vpos);
 milestone("v2.0.x branch from v2.x", "2016-09-13", $data, $vpos);
-#milestone("v2.1.0", "2016-07-12", $data, $vpos);
+milestone("v2.1.0", "2017-03-21", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
