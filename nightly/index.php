679bf29e4bdb9e16f7f47e11fddc89d0b39638df
Jeff Squyres <jsquyres@cisco.com>
2017-04-25 16:58:01 -0700

    Rename nightly snapshots from v3.x to v3.0.x.
    Put in a sym link so that "v3.x" continues to work (for a while).
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- nightly/index.php
+++ nightly/index.php
@@ -24,8 +24,8 @@ for all users to download and use.</font></strong></li>
 
 <p> <strong>Upcoming release series</strong>
 
-<li><a href="v3.x/"><strong>v3.x series</strong></a>: These are images from
-the v3.x series (i.e., what will become v3.0.0).
+<li><a href="v3.0.x/"><strong>v3.0.x series</strong></a>: These are images from
+the v3.0.x series (i.e., what will become v3.0.0).
 <strong><font color="red">This branch is new and not yet fully tested.
 It is only recomended for users who wish to test the upcoming
 release.</font></strong></li>
