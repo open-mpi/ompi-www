bfde271aaa14cf29a7c5427b4ecf85bff85671e6
Howard Pritchard <howardp@lanl.gov>
2017-05-10 12:18:14 -0600

    release 2.1.1
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- software/ompi/v2.1/index.php
+++ software/ompi/v2.1/index.php
@@ -94,7 +94,7 @@ $t_older = new downloadTable("./downloads", "./downloads", dirname($_SERVER["PHP
 
 if (isset($t_older)) {
     $versions = array();
-#    $versions[] = "2.1.0";
+    $versions[] = "2.1.0";
 
     foreach ($versions as $v) {
         $n = array();
