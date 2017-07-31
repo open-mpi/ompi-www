1cf28ba9fcc94e84e89844cdbbae78f360a766f1
Jeff Squyres <jsquyres@cisco.com>
2017-07-31 13:14:12 -0700

    sc17: add BOF question form
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>
    
    Former-commit-id: 1fed05ed3a374e8a97f09b98014948c0e828fc75

---
+++ sc17/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$topdir = "..";
+$title = "Open MPI State of the Union XI BOF at SC'17";
+$short_title = "OMPI SotU BOF@SC'17";
+
+include_once("$topdir/includes/header.inc");
+
+?>
+
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfbubS-ldcfAnpXmF75EtH6g28Nh-hL1FY9IJc2cLX6brqBWA/viewform?embedded=true" width="760" height="1500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
+<?php
+include_once("$topdir/includes/footer.inc");
