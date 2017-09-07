80b12db1f8bda913b9d0e0df48518e747182bbdf
Brian Barrett <bbarrett@amazon.com>
2017-09-07 20:26:39 +0000

    Move release artifacts to S3
    To shrink the size of the ompi-www repo and enable CloudFront
    for tarball download, we're moving the release artifacts
    (tarballs, Windows packages, etc.) to S3.  This commit does not
    remove the tarballs (nor require that they be removed) but
    uses S3 as the source of truth, ignoring any tarballs in the repo.
    
    Since we can no longer use `ls` to get the list of available
    tarballs, also rewrite the table generation code a little bit
    so that adding a release or pre-release is as simple as
    updating the correct array in version.inc.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>
    
    Former-commit-id: 2afe36cda1054f90c8084c2a2e68e01971c676b5

--- software/ompi/v1.1/index.php
+++ software/ompi/v1.1/index.php
@@ -1,125 +1,17 @@
 <?php
 $topdir = "../../..";
 
-include_once("$topdir/software/ompi/v1.1/version.inc");
-$title = "Open MPI: Version $ver_v1_1";
+include_once("version.inc");
+$title = "Open MPI: Version $release_series";
 
 include_once("$topdir/software/ompi/nav.inc");
 include_once("$topdir/includes/header.inc");
 include_once("$topdir/includes/downloads.inc");
 
-$md5 = read_checksums("downloads/md5sums.txt");
-$sha1 = read_checksums("downloads/sha1sums.txt");
+$project = "Open MPI";
+$list_name = "announce";
+$prev_describe = "the v$release_series download page";
 
-#############################################################################
-#
-# Current stable release
-#
-#############################################################################
-
-$t_stable = new downloadTable("./downloads", "./downloads",
-			      dirname($_SERVER["PHP_SELF"]));
-
-$src = "Version $ver_v1_1<br><a href=\"srpm.php\">SRPM notes</a>";
-$name = "openmpi-$ver_v1_1.tar";
-$t_stable->addFile($src, "$name.bz2", $md5["$name.bz2"], $sha1["$name.bz2"]);
-$t_stable->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-$name = "openmpi-$ver_v1_1-1.src.rpm";
-$t_stable->addFile($src, $name, $md5[$name], $sha1[$name]);
-
-# Comment out until Brian has a chance to make the OSX package
-#$src = "v$ver_v1_1 (stable) OS X Package<BR> (Tiger/10.4, <a href=\"macosx.php\">Package notes</a>)";
-#$name = "openmpi-$ver_v1_1.dmg.gz";
-#$t_stable->addFile($src, $name, $md5[$name], $sha1[$name]);
-
-#############################################################################
-#
-# Prereleases
-#
-#############################################################################
-
-#$t_prerelease = new downloadTable("./downloads", "./downloads",
-#				  dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_prerelease)) {
-    $fp = fopen("downloads/latest_snapshot.txt", "r");
-    $v = fgets($fp);
-    fclose($fp);
-    $v = substr($v, 0, strlen($v) - 1);
-    $s = "$v pre-release";
-
-    $name = "openmpi-$v.tar";
-    $t_prerelease->addFile($s, "$name.bz2", $md5["$name.bz2"],
-			   $sha1["$name.bz2"]);
-    $t_prerelease->addFile($s, "$name.gz", $md5["$name.gz"], 
-			   $sha1["$name.gz"]);
-}
-
-#############################################################################
-#
-# Older releases
-#
-#############################################################################
-
-$t_older = new downloadTable("./downloads", "./downloads",
-			     dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_older)) {
-    $ver = "1.1.4";
-    $src = "Version $ver<br><a href=\"srpm.php\">SRPM notes</a>";
-    $name = "openmpi-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"],
-		      $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-    $name = "openmpi-$ver-1.src.rpm";
-    $t_older->addFile($src, $name, $md5[$name], $sha1[$name]);
-    
-    $ver = "1.1.3";
-    $src = "Version $ver<br><a href=\"srpm.php\">SRPM notes</a>";
-    $name = "openmpi-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"],
-		      $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-    $name = "openmpi-$ver-1.src.rpm";
-    $t_older->addFile($src, $name, $md5[$name], $sha1[$name]);
-    
-    $ver = "1.1.2";
-    $src = "Version $ver<br><a href=\"srpm.php\">SRPM notes</a>";
-    $name = "openmpi-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"],
-		      $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-    $name = "openmpi-$ver-1.src.rpm";
-    $t_older->addFile($src, $name, $md5[$name], $sha1[$name]);
-    
-    $ver = "1.1.1";
-    $src = "Version $ver<br><a href=\"srpm.php\">SRPM notes</a>";
-    $name = "openmpi-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"],
-		      $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-    $name = "openmpi-$ver-1.src.rpm";
-    $t_older->addFile($src, $name, $md5[$name], $sha1[$name]);
-    
-    $ver = "1.1";
-    $src = "Version $ver<br><a href=\"srpm.php\">SRPM notes</a><br><a href=\"macosx.php\">OS X package notes</a>";
-    $name = "openmpi-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"], 
-		      $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-    $name = "openmpi-$ver-1.src.rpm";
-    $t_older->addFile($src, $name, $md5[$name], $sha1[$name]);
-    $name = "openmpi-$ver.dmg.gz";
-    $t_older->addFile($src, $name, $md5[$name], $sha1[$name]);
-}
-
-#############################################################################
-#
-# Main display part of the page
-#
-#############################################################################
-
-$prev_describe = "the v$ver_v1_1 download page";
 include_once("$topdir/includes/subscribe-announce.inc");
 ?>
 
@@ -134,33 +26,24 @@ possible.</strong></p>
 
 <p><dv align=center><hr width=50%></div></p>
 
-<P><?php $dir = "https://github.com/open-mpi/ompi/raw/v1.1/NEWS";
+<P><?php $dir = "https://github.com/open-mpi/ompi/raw/$release_branch/NEWS";
       print("<a href=\"$dir\">"); ?>This
 file</a> contains a list of changes between the releases in the Open
-MPI v1.1 series.</p>
+MPI in the v<?php print("$release_series"); ?> series.</p>
 
-<p>See the 
-<a href="<?php print($topdir); ?>/software/ompi/versions/timeline.php">version
-timeline</a> for information on the chronology of Open MPI releases.</p>
-
-<p>Current stable release:</p>
+<p>See the <a href="<?php print($topdir);
+?>/software/ompi/versions/timeline.php">version timeline</a> for
+information on the chronology of Open MPI releases.</p>
 
 <p>
 <div align="center">
 
 <?php
-$t_stable->printMe();
-if (isset($t_prerelease)) {
-    print("</div>\n\n<p>Prerelease:</p>\n\n<p>\n<div align=center>\n\n");
-     $t_prerelease->printMe();
-}
-if (isset($t_older)) {
-    print("</div>\n\n<p>Previous releases:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_older->printMe();
-}
+print_release_section("openmpi", "open-mpi-release", $s3_prefix, $download_prefix,
+                       $releases, $prereleases, $cygwin_note);
 ?>
 </div>
 </p>
 
-<?php 
+<?php
   include_once("$topdir/includes/footer.inc");
