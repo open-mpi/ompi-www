f64319359be545288b4798efc56beaa82a62e81b
Howard Pritchard <howardp@lanl.gov>
2017-09-20 14:55:16 -0600

    Release v2.1.2
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- index.php
+++ index.php
@@ -87,6 +87,9 @@ information</a></h3>
 
 <?php
 # Lists are not mirrored - use an absolute URL
+news("Open MPI v2.1.2 released",
+     "Bug fix release",
+     "https://www.mail-archive.com/announce@lists.open-mpi.org//msg00103.html");
 news("Open MPI v2.0.3 released",
      "Bug fix release",
      "https://www.mail-archive.com/announce@lists.open-mpi.org//msg00101.html");
