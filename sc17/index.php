ef8bbda41b7e5f949f351a117ebe4dae3d89a491
Jeff Squyres <jsquyres@cisco.com>
2017-11-13 14:28:20 -0800

    sc17: add missing </a>
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- sc17/index.php
+++ sc17/index.php
@@ -10,7 +10,7 @@ include_once("$topdir/includes/header.inc");
 
 <h2><a href="http://sc17.supercomputing.org/presentation/?id=bof115&sess=sess328">BOF session page (including the link to ask a question / vote on questions)</a></h2>
 
-<h2><a href="https://submissions.supercomputing.org/?page=SessionEval&new_year=sc17&id=sess328&eval_stype=stype159"></a>BOF evaluation form</h2>
+<h2><a href="https://submissions.supercomputing.org/?page=SessionEval&new_year=sc17&id=sess328&eval_stype=stype159"></a>BOF evaluation form</a></h2>
 
 <?php
 include_once("$topdir/includes/footer.inc");
