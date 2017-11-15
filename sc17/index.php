0cc84e89ac6c7a52798d723dc781f6f0d4ae3fb9
Jeff Squyres <jsquyres@cisco.com>
2017-11-15 05:58:18 -0800

    Add Linklings link
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>
    
    Former-commit-id: 2f60b0d4ee4448212c0078a36c9dc99ecb6c3d5d

--- sc17/index.php
+++ sc17/index.php
@@ -8,7 +8,9 @@ include_once("$topdir/includes/header.inc");
 
 ?>
 
-<h2><a href="http://sc17.supercomputing.org/presentation/?id=bof115&sess=sess328">BOF session page (including the link to ask a question / vote on questions)</a></h2>
+<h2><a href="http://sc17.supercomputing.org/presentation/?id=bof115&sess=sess328">BOF session page</a></h2>
+
+<h2><a href="https://qna.live/sc17/Ym9mMTE1/">Ask a question online</a></h2>
 
 <h2><a href="https://submissions.supercomputing.org/?page=SessionEval&new_year=sc17&id=sess328&eval_stype=stype159">BOF evaluation form</a></h2>
 
