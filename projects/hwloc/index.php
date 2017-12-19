57a66e3687d422b71fe789f99ead44c92c154c26
Brice Goglin <Brice.Goglin@inria.fr>
2017-12-19 11:52:55 +0100

    hwloc: fix v2b1 mail link
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    
    Former-commit-id: 122566a1356814e5487d05f1cd432de7608989a0

--- projects/hwloc/index.php
+++ projects/hwloc/index.php
@@ -18,7 +18,7 @@ include_once("$topdir/includes/code.inc");
 # Lists are not mirrored - use an absolute URL
 news("hwloc v2.0 beta1 released",
      "Experimental major release",
-     "https://mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00101.html");
+     "https://mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00102.html");
 news("hwloc v1.11.8 released",
      "Stable release",
      "https://mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00100.html");
