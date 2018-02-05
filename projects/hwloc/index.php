bbd58b8345518519e3112c406a288d6072de2706
Brice Goglin <Brice.Goglin@inria.fr>
2018-02-05 22:22:06 +0100

    hwloc: update the news link to 2.0.0 announce
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- projects/hwloc/index.php
+++ projects/hwloc/index.php
@@ -18,7 +18,7 @@ include_once("$topdir/includes/code.inc");
 # Lists are not mirrored - use an absolute URL
 news("hwloc v2.0.0 released",
      "Major release",
-     "https://mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00106.html");
+     "https://mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00108.html");
 news("Upgrading to v2.0 API",
      "Guide for Porting your Code",
      "doc/v2.0.0/a00327.php");
