d228a5d69930d3514494b8a0edb00f7b76845ce1
Jeff Squyres <jsquyres@cisco.com>
2018-03-20 16:07:33 -0700

    contribution: remove obsolete SLA docs
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>
    
    Former-commit-id: d0d5f38a79051df96b643989ba1db73ebc8b5e48

--- community/contribute/corporate.php
+++ community/contribute/corporate.php
@@ -1,27 +1,2 @@
 <?php
-  $topdir = "../..";
-  $title = "Contributing to Open MPI as an Organization";
-
-  include_once("nav.inc");
-  include_once("$topdir/includes/header.inc");
-?>
-
-<p><em> Please also see the FAQ under the catrgory "<a href="<?php
-print($topdir); ?>/faq/?category=contributing">Contributing to Open
-MPI</a>" for more information.</em>
-
-<p> If you are an organization whose employees will be submitting code
-to Open MPI, you need to fill out an Open MPI Corporate Contributor
-License Agreement form.
-
-<ul>
-
-<li> <a href="open-mpi-corporate-contributor-agrement.pdf">PDF</a></li>
-
-</ul>
-
-Instructions are provided on the form how to submit it to the Open MPI
-project.
-
-<?php 
-  include_once("$topdir/includes/footer.inc"); 
+header("Location: .");
