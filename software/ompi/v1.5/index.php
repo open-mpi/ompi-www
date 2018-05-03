0a9b8d3ba20ad3651ced0000cc1bdea636045bb0
Brian Barrett <bbarrett@amazon.com>
2018-05-03 17:20:34 +0000

    Move release artifacts to S3 (take two)
    To shrink the size of the ompi-www repo and enable CloudFront
    for tarball download, we're moving the release artifacts
    (tarballs, Windows packages, etc.) to S3.  This commit does not
    remove the tarballs (nor require that they be removed) but
    uses S3 as the source of truth, ignoring any tarballs in the repo.
    
    Since we can no longer use `ls` to get the list of available
    tarballs, also rewrite the table generation code a little bit
    so that adding a release or pre-release is as simple as
    updating the correct array in version.inc.
    
    This is take two of the patch which will deal with anonymous
    S3 access properly for mirrors and rebases on top of the HEAD
    of the web tree.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- software/ompi/v1.5/index.php
+++ software/ompi/v1.5/index.php
@@ -1,164 +1,26 @@
 <?php
 $topdir = "../../..";
 
-include_once("$topdir/software/ompi/v1.5/version.inc");
-$title = "Open MPI: Version $ver_v1_5";
+include_once("version.inc");
+$title = "Open MPI: Version $release_series";
 
 include_once("$topdir/software/ompi/nav.inc");
 include_once("$topdir/includes/header.inc");
 include_once("$topdir/includes/downloads.inc");
 
-$md5 = read_checksums("downloads/md5sums.txt");
-$sha1 = read_checksums("downloads/sha1sums.txt");
-
-$notes = "<a href=\"srpm.php\">SRPM notes</a><br><a href=\"ms-windows.php\">MS Windows notes</a>";
-
-#############################################################################
-#
-# Current stable release
-#
-#############################################################################
-
-$t_stable = new downloadTable("./downloads", "./downloads", dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_stable)) {
-    $src = "Version $ver_v1_5<br>$notes";
-
-    $n[] = "openmpi-$ver_v1_5.tar.bz2";
-    $n[] = "openmpi-$ver_v1_5.tar.gz";
-    $n[] = "openmpi-$ver_v1_5-2.src.rpm";
-    $n[] = "openmpi-$ver_v1_5-1.dmg.gz";
-    $n[] = "openmpi-$ver_v1_5-1.dmg.bz2";
-    $n[] = "OpenMPI_v$ver_v1_5-1_win32.exe";
-    $n[] = "OpenMPI_v$ver_v1_5-1_win64.exe";
-    # Need this for some versions
-    $n[] = "OpenMPI_v$ver_v1_5-2_win32.exe";
-    $n[] = "OpenMPI_v$ver_v1_5-2_win64.exe";
-
-    foreach ($n as $name) {
-        if (file_exists("downloads/$name")) {
-            $t_stable->addFile($src, $name, $md5[$name], $sha1[$name]);
-        }
-    }
-
-    $t_stable->addLink("Oracle Message Passing Toolkit<br />(powered by Open MPI)", 
-               "Oracle Message Passing Toolkit<br />for Solaris and Linux",
-               "2010",
-               "http://www.oracle.com/goto/ompt"
-               );
-}
-
-#############################################################################
-#
-# Prereleases
-#
-#############################################################################
-
-# See if there's a snapshot file that does not have any letters in its
-# version number (i.e., is not a stable release) and has a
-# corresponding .tar.bz2 file or .tar.gz here in the downloads
-# directory.
-
-$fp = fopen("downloads/latest_snapshot.txt", "r");
-$v = fgets($fp);
-fclose($fp);
-if (strlen($v) > 0) {
-    $v = substr($v, 0, strlen($v) - 1);
-}
-
-if (preg_match("/[a-z]/i", $v) &&
-    (file_exists("downloads/openmpi-$v.tar.bz2") ||
-     file_exists("downloads/openmpi-$v.tar.gz") ||
-     file_exists("downloads/openmpi-$v-1.src.rpm"))) {
-    $t_prerelease = new downloadTable("./downloads", "./downloads", 
-                                      dirname($_SERVER["PHP_SELF"]));
-
-    $s = "$v (pre-release)<br>$notes";
-
-    $n = array();
-    $n[] = "openmpi-$v.tar.bz2";
-    $n[] = "openmpi-$v.tar.gz";
-    $n[] = "openmpi-$v-1.src.rpm";
-    $n[] = "openmpi-$v-1.dmg.gz";
-    $n[] = "openmpi-$v-1.dmg.bz2";
-    $n[] = "OpenMPI_v$v-1_win32.exe";
-    $n[] = "OpenMPI_v$v-1_win64.exe";
-
-    foreach ($n as $name) {
-        if (file_exists("downloads/$name")) {
-            $t_prerelease->addFile($s, $name, $md5[$name], $sha1[$name]);
-        }
-    }
-}
-
-#############################################################################
-#
-# Older releases
-#
-#############################################################################
-
-$t_older = new downloadTable("./downloads", "./downloads", dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_older)) {
-    $versions = array();
-    $versions[] = "1.5.4";
-    $versions[] = "1.5.3";
-    $versions[] = "1.5.2";
-    $versions[] = "1.5.1";
-    $versions[] = "1.5";
-
-    foreach ($versions as $v) {
-        $n = array();
-        $n[] = "openmpi-$v.tar.bz2";
-        $n[] = "openmpi-$v.tar.gz";
-        $n[] = "openmpi-$v-1.dmg.gz";
-        $n[] = "openmpi-$v-1.dmg.bz2";
-        $n[] = "openmpi-$v-1.src.rpm";
-        $n[] = "OpenMPI_v$v-1_win32.exe";
-        $n[] = "OpenMPI_v$v-1_win64.exe";
-        # Need this for some versions
-        $n[] = "OpenMPI_v$v-2_win32.exe";
-        $n[] = "OpenMPI_v$v-2_win64.exe";
-
-        $s = "Version $v<br>$notes";
-
-        foreach ($n as $name) {
-            if (file_exists("downloads/$name")) {
-                $t_older->addFile($s, $name, $md5[$name], $sha1[$name]);
-            }
-        }
-    }
-}
-
-#############################################################################
-#
-# Main display part of the page
-#
-#############################################################################
-
 $project = "Open MPI";
 $list_name = "announce";
-$prev_describe = "the v$ver_v1_5 download page";
+$prev_describe = "the v$release_series download page";
+
 include_once("$topdir/includes/subscribe-announce.inc");
 ?>
 
 <p><hr>
 
-<p><strong><font color=red>PLEASE NOTE:</font></strong> According to
-<a href="<?php print($topdir); ?>/software/ompi/versions/">Open MPI's
-release methodology</a>, the v1.5 series is a "feature release"
-series.  This means that it has rich new features that we think are
-tested and stable, but they are not as mature as the stable "bug fix"
-v1.4 series.  If you are looking for ultra stability, you should
-probably be using the <a href="<?php print($topdir);
-?>/software/ompi/v1.4/">v1.4 series</a>.</p>
-
-<p><dv align=center><hr width=50%></div></p>
-
-<P><?php $dir = "https://github.com/open-mpi/ompi/raw/v1.5/NEWS";
+<P><?php $dir = "https://github.com/open-mpi/ompi/raw/$release_branch/NEWS";
       print("<a href=\"$dir\">"); ?>This
 file</a> contains a list of changes between the releases in the Open
-MPI releases in the v1.5 series</p>
+MPI in the v<?php print("$release_series"); ?> series.</p>
 
 <p>See the <a href="<?php print($topdir);
 ?>/software/ompi/versions/timeline.php">version timeline</a> for
@@ -168,21 +30,11 @@ information on the chronology of Open MPI releases.</p>
 <div align="center">
 
 <?php
-if (isset($t_stable)) {
-    print("</div>\n\n<p>Current stable release:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_stable->printMe();
-}
-if (isset($t_prerelease)) {
-    print("</div>\n\n<p>Prerelease:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_prerelease->printMe();
-}
-if (isset($t_older)) {
-    print("</div>\n\n<p>Previous releases:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_older->printMe();
-}
+print_release_section("openmpi", "open-mpi-release", $s3_prefix, $download_prefix,
+                       $releases, $prereleases, $cygwin_note);
 ?>
 </div>
 </p>
 
-<?php 
+<?php
   include_once("$topdir/includes/footer.inc");
--- software/ompi/v1.8/index.php
+++ software/ompi/v1.8/index.php
@@ -1,138 +1,26 @@
 <?php
 $topdir = "../../..";
 
-include_once("$topdir/software/ompi/v1.8/version.inc");
-$title = "Open MPI: Version $ver_v1_8";
+include_once("version.inc");
+$title = "Open MPI: Version $release_series";
 
 include_once("$topdir/software/ompi/nav.inc");
 include_once("$topdir/includes/header.inc");
 include_once("$topdir/includes/downloads.inc");
 
-$md5 = read_checksums("downloads/md5sums.txt");
-$sha1 = read_checksums("downloads/sha1sums.txt");
-
-$notes = "<a href=\"srpm.php\">SRPM notes</a>";
-
-#############################################################################
-#
-# Current stable release
-#
-#############################################################################
-
-$t_stable = new downloadTable("./downloads", "./downloads", dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_stable)) {
-    $src = "Version $ver_v1_8<br>$notes";
-
-    $n[] = "openmpi-$ver_v1_8.tar.bz2";
-    $n[] = "openmpi-$ver_v1_8.tar.gz";
-    $n[] = "openmpi-$ver_v1_8-1.src.rpm";
-
-    foreach ($n as $name) {
-        if (file_exists("downloads/$name")) {
-            $t_stable->addFile($src, $name, $md5[$name], $sha1[$name]);
-        }
-    }
-
-    $t_stable->addLink("Cygwin support", 
-		       "Precompiled Open MPI packages for Cygwin",
-		       date("Y"),
-		       "http://cygwin.com/cgi-bin2/package-grep.cgi?grep=openmpi"
-		       );
-}
-
-#############################################################################
-#
-# Prereleases
-#
-#############################################################################
-
-# See if there's a snapshot file that does not have any letters in its
-# version number (i.e., is not a stable release) and has a
-# corresponding .tar.bz2 file or .tar.gz here in the downloads
-# directory.
-
-$fp = fopen("downloads/latest_snapshot.txt", "r");
-$v = fgets($fp);
-fclose($fp);
-if (strlen($v) > 0) {
-    $v = substr($v, 0, strlen($v) - 1);
-}
-
-if (preg_match("/[a-z]/i", $v) &&
-    (file_exists("downloads/openmpi-$v.tar.bz2") ||
-     file_exists("downloads/openmpi-$v.tar.gz") ||
-     file_exists("downloads/openmpi-$v-1.src.rpm"))) {
-    $t_prerelease = new downloadTable("./downloads", "./downloads", 
-                                      dirname($_SERVER["PHP_SELF"]));
-
-    $s = "$v (pre-release)<br>$notes";
-
-    $n = array();
-    $n[] = "openmpi-$v.tar.bz2";
-    $n[] = "openmpi-$v.tar.gz";
-    $n[] = "openmpi-$v-1.src.rpm";
-
-    foreach ($n as $name) {
-        if (file_exists("downloads/$name")) {
-            $t_prerelease->addFile($s, $name, $md5[$name], $sha1[$name]);
-        }
-    }
-}
-
-#############################################################################
-#
-# Older releases
-#
-#############################################################################
-
-$t_older = new downloadTable("./downloads", "./downloads", dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_older)) {
-    $versions = array();
-    $versions[] = "1.8.7";
-    $versions[] = "1.8.6";
-    $versions[] = "1.8.5";
-    $versions[] = "1.8.4";
-    $versions[] = "1.8.3";
-    $versions[] = "1.8.2";
-    $versions[] = "1.8.1";
-    $versions[] = "1.8";
-
-    foreach ($versions as $v) {
-        $n = array();
-        $n[] = "openmpi-$v.tar.bz2";
-        $n[] = "openmpi-$v.tar.gz";
-        $n[] = "openmpi-$v-1.src.rpm";
-
-        $s = "Version $v<br>$notes";
-
-        foreach ($n as $name) {
-            if (file_exists("downloads/$name")) {
-                $t_older->addFile($s, $name, $md5[$name], $sha1[$name]);
-            }
-        }
-    }
-}
-
-#############################################################################
-#
-# Main display part of the page
-#
-#############################################################################
-
 $project = "Open MPI";
 $list_name = "announce";
-$prev_describe = "the v$ver_v1_6 download page";
+$prev_describe = "the v$release_series download page";
+
 include_once("$topdir/includes/subscribe-announce.inc");
 ?>
 
 <p><hr>
 
-<P><?php $dir = "https://github.com/open-mpi/ompi/raw/v1.8/NEWS";
+<P><?php $dir = "https://github.com/open-mpi/ompi/raw/$release_branch/NEWS";
       print("<a href=\"$dir\">"); ?>This
 file</a> contains a list of changes between the releases in the Open
-MPI releases in the v1.8 series</p>
+MPI in the v<?php print("$release_series"); ?> series.</p>
 
 <p>See the <a href="<?php print($topdir);
 ?>/software/ompi/versions/timeline.php">version timeline</a> for
@@ -142,21 +30,11 @@ information on the chronology of Open MPI releases.</p>
 <div align="center">
 
 <?php
-if (isset($t_stable)) {
-    print("</div>\n\n<p>Current stable release:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_stable->printMe();
-}
-if (isset($t_prerelease)) {
-    print("</div>\n\n<p>Prerelease:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_prerelease->printMe();
-}
-if (isset($t_older)) {
-    print("</div>\n\n<p>Previous releases:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_older->printMe();
-}
+print_release_section("openmpi", "open-mpi-release", $s3_prefix, $download_prefix,
+                       $releases, $prereleases, $cygwin_note);
 ?>
 </div>
 </p>
 
-<?php 
+<?php
   include_once("$topdir/includes/footer.inc");
