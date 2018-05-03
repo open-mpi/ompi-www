fa75a61f0d909dab7f78cb95fa3dd12541e0c39d
Brian Barrett <bbarrett@amazon.com>
2018-05-03 17:20:34 +0000

    Move release artifacts to S3 (take two)
    To shrink the size of the ompi-www repo and enable CloudFront
    for tarball download, we're moving the release artifacts
    (tarballs, Windows packages, etc.) to S3.  This commit does not
    remove the tarballs (nor require that they be removed) but
    uses S3 as the source of truth, ignoring any tarballs in the repo.
    
    Since we can no longer use `ls` to get the list of available
    tarballs, also rewrite the table generation code a little bit
    so that adding a release or pre-release is as simple as
    updating the correct array in version.inc.
    
    This is take two of the patch which will deal with anonymous
    S3 access properly for mirrors and rebases on top of the HEAD
    of the web tree.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- software/plpa/v1.3/index.php
+++ software/plpa/v1.3/index.php
@@ -1,109 +1,17 @@
 <?php
 $topdir = "../../..";
 
-include_once("$topdir/software/plpa/v1.3/version.inc");
-$title = "Portable Linux Processor Affinity (PLPA): Version $ver_v1_3";
+include_once("version.inc");
+$title = "Portable Linux Processor Affinity (PLPA): Version $release_series";
 
 include_once("$topdir/projects/plpa/nav.inc");
 include_once("$topdir/includes/header.inc");
 include_once("$topdir/includes/downloads.inc");
 
-$md5 = read_checksums("downloads/md5sums.txt");
-$sha1 = read_checksums("downloads/sha1sums.txt");
-
-#############################################################################
-#
-# Current stable release
-#
-#############################################################################
-
-$t_stable = new downloadTable("./downloads", "./downloads", 
-			      dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_stable)) {
-    $src = "Version $ver_v1_3";
-    $name = "plpa-$ver_v1_3.tar";
-    $t_stable->addFile($src, "$name.bz2", $md5["$name.bz2"], $sha1["$name.bz2"]);
-    $t_stable->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-}
-
-#############################################################################
-#
-# Prereleases
-#
-#############################################################################
-
-#$t_prerelease = new downloadTable("./downloads", "./downloads",
-#				  dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_prerelease)) {
-    $s = "v1.3 pre-release";
-    $fp = fopen("downloads/latest_snapshot.txt", "r");
-    $v = fgets($fp);
-    fclose($fp);
-    $v = substr($v, 0, strlen($v) - 1);
-
-    $name = "plpa-$v.tar";
-    $t_prerelease->addFile($s, "$name.bz2", $md5["$name.bz2"], 
-			   $sha1["$name.bz2"]);
-    $t_prerelease->addFile($s, "$name.gz", $md5["$name.gz"], 
-			   $sha1["$name.gz"]);
-}
-
-#############################################################################
-#
-# Older releases
-#
-#############################################################################
-
-$t_older = new downloadTable("./downloads", "./downloads",
-			     dirname($_SERVER["PHP_SELF"]));
-
-if (isset($t_older)) {
-    $ver = "1.3.1";
-    $src = "Version $ver";
-    $name = "plpa-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"], $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-
-    $ver = "1.3";
-    $src = "Version $ver";
-    $name = "plpa-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"], $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-}
-
-#############################################################################
-#
-# Main display part of the page
-#
-#############################################################################
-
 $project = "PLPA";
-include_once("$topdir/projects/plpa/deprecated.inc"); 
-?>
+include_once("$topdir/projects/plpa/deprecated.inc");
 
-<P><?php $dir = "svn.open-mpi.org/svn/plpa/branches/$ver_v1_3_dir/NEWS";
-      print("<a href=\"http://$dir\">"); ?>This
-file</a> contains a list of changes between the releases in the PLPA
-releases in the v<?php print($ver_v1_3); ?> series</p>
+print_release_section("plpa", "open-mpi-release", $s3_prefix, $download_prefix,
+                      $releases, $prereleases, FALSE);
 
-<?php
-if (isset($t_stable)) {
-    print("<p>Current stable release:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_stable->printMe();
-}
-if (isset($t_prerelease)) {
-    print("</div>\n\n<p>Prerelease:</p>\n\n<p>\n<div align=center>\n\n");
-     $t_prerelease->printMe();
-}
-if (isset($t_older)) {
-    print("</div>\n\n<p>Previous releases:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_older->printMe();
-}
-?>
-</div>
-</p>
-
-<?php 
-  include_once("$topdir/includes/footer.inc");
+include_once("$topdir/includes/footer.inc");
