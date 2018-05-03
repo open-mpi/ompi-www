fae012ba74bb5b4ab83080cd6643741b15365948
Brian Barrett <bbarrett@amazon.com>
2018-05-03 17:20:34 +0000

    Move release artifacts to S3 (take two)
    To shrink the size of the ompi-www repo and enable CloudFront
    for tarball download, we're moving the release artifacts
    (tarballs, Windows packages, etc.) to S3.  This commit does not
    remove the tarballs (nor require that they be removed) but
    uses S3 as the source of truth, ignoring any tarballs in the repo.
    
    Since we can no longer use `ls` to get the list of available
    tarballs, also rewrite the table generation code a little bit
    so that adding a release or pre-release is as simple as
    updating the correct array in version.inc.
    
    This is take two of the patch which will deal with anonymous
    S3 access properly for mirrors and rebases on top of the HEAD
    of the web tree.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>
    
    Former-commit-id: 0a9b8d3ba20ad3651ced0000cc1bdea636045bb0

--- software/hwloc/v0.9/index.php
+++ software/hwloc/v0.9/index.php
@@ -1,98 +1,16 @@
 <?php
 $topdir = "../../..";
 
-include_once("$topdir/software/hwloc/v0.9/version.inc");
-$title = "Portable Hardware Locality (hwloc): Version $ver_v0_9";
+include_once("version.inc");
+$title = "Portable Hardware Locality (hwloc): Version $release_series";
 
 include_once("$topdir/projects/hwloc/nav.inc");
 include_once("$topdir/includes/header.inc");
 include_once("$topdir/includes/downloads.inc");
 
-$md5 = read_checksums("downloads/md5sums.txt");
-$sha1 = read_checksums("downloads/sha1sums.txt");
-
-#############################################################################
-#
-# Current stable release
-#
-#############################################################################
-
-$t_stable = new downloadTable("./downloads", "./downloads",
-			      dirname($_SERVER["PHP_SELF"]), 0);
-
-if (isset($t_stable)) {
-    $src = "Version $ver_v0_9";
-    $name = "hwloc-$ver_v0_9.tar";
-    $t_stable->addFile($src, "$name.bz2", $md5["$name.bz2"], $sha1["$name.bz2"]);
-    $t_stable->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-    $name = "hwloc-win32-build-$ver_v0_9.zip";
-    $t_stable->addFile($src, $name, $md5[$name], $sha1[$name]);
-}
-
-#############################################################################
-#
-# Prereleases
-#
-#############################################################################
-
-#$t_prerelease = new downloadTable("./downloads", "./downloads",
-#				  dirname($_SERVER["PHP_SELF"]), 0);
-
-if (isset($t_prerelease)) {
-    $s = "v0.9 pre-release";
-    $fp = fopen("downloads/latest_snapshot.txt", "r");
-    $v = fgets($fp);
-    fclose($fp);
-    $v = substr($v, 0, strlen($v) - 1);
-
-    $name = "hwloc-$v.tar";
-    $t_prerelease->addFile($s, "$name.bz2", $md5["$name.bz2"], 
-			   $sha1["$name.bz2"]);
-    $t_prerelease->addFile($s, "$name.gz", $md5["$name.gz"], 
-			   $sha1["$name.gz"]);
-}
-
-#############################################################################
-#
-# Older releases
-#
-#############################################################################
-
-$t_older = new downloadTable("./downloads", "./downloads",
-			     dirname($_SERVER["PHP_SELF"]), 0);
-
-if (isset($t_older)) {
-    $ver = "0.9.2";
-    $src = "Version $ver";
-    $name = "hwloc-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"], $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-    $name = "hwloc-win32-build-$ver.zip";
-    $t_older->addFile($src, $name, $md5[$name], $sha1[$name]);
-
-    $ver = "0.9.1";
-    $src = "Version $ver";
-    $name = "hwloc-$ver.tar";
-    $t_older->addFile($src, "$name.bz2", $md5["$name.bz2"], $sha1["$name.bz2"]);
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-
-    $ver = "0.9";
-    $src = "Libtopology v$ver";
-    $name = "libtopology-$ver.tar";
-    $t_older->addFile($src, "$name.gz", $md5["$name.gz"], $sha1["$name.gz"]);
-    $name = "libtopology-win32-build-$ver";
-    $t_older->addFile($src, "$name.zip", $md5["$name.zip"], $sha1["$name.zip"]);
-}
-
-#############################################################################
-#
-# Main display part of the page
-#
-#############################################################################
-
 $project = "hwloc";
 $list_name = "hwloc-announce";
-$prev_describe = "the v$ver_v0_9 download page";
+$prev_describe = "the v$release_series download page";
 include_once("$topdir/includes/subscribe-announce.inc");
 ?>
 
@@ -101,35 +19,26 @@ include_once("$topdir/includes/subscribe-announce.inc");
 <p>hwloc is distributed under the <a href="<?php print $topdir ?>/projects/hwloc/license.php">BSD license</a>.
 </p>
 
-<P><?php print("<a href=\"https://raw.github.com/open-mpi/hwloc/$ver_v0_9_dir/NEWS\">"); ?>This
+<P><?php print("<a href=\"https://raw.github.com/open-mpi/hwloc/$release_branch/NEWS\">"); ?>This
 file</a> contains a list of changes between the releases in the hwloc
-releases in the <?php print($ver_v0_9_dir); ?> series</p>
+releases in the <?php print($release_series); ?> series</p>
 
 <?php
-$found = 0;
-if (isset($t_stable)) {
-    print("<p>Current stable release:</p>\n\n<p>\n<div align=center>\n\n");
-    $t_stable->printMe();
-    $found = 1;
-}
-if (isset($t_prerelease)) {
-    print("</div>\n\n<p>Prerelease:</p>\n\n<p>\n<div align=center>\n\n");
-     $t_prerelease->printMe();
-    $found = 1;
-}
+print_release_section("hwloc", "open-mpi-release", $s3_prefix, $download_prefix,
+                      $releases, $prereleases, FALSE);
 
-if (0 == $found) {
-    print("<p><strong>No hwloc releases yet -- come back soon!</strong></p>\n");
-}
+print("</div>\n\n<p>hwloc was called libtopology prior to the 0.9.1 release.  " .
+      "Previous <A HREF=\"https://www.open-mpi.org/software/plpa/\">versions " . 
+      "of PLPA</a> are also available.\n<div align=center>\n\n");
 
-if (isset($t_older)) {
-    print("</div>\n\n<p>Previous releases (including libtopology; <a href=\"$topdir/software/plpa/\">previous PLPA releases</a> are also available):</p>\n\n<p>\n<div align=center>\n\n");
-    $t_older->printMe();
-}
+print_release_table("libtopology", ["0.9"], "open-mpi-release",
+                    $s3_prefix, $download_prefix);
+
+print("</div>\n");
 
 ?>
 </div>
 </p>
 
-<?php 
+<?php
   include_once("$topdir/includes/footer.inc");
