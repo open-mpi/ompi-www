36dd7417dc698effa2ffa411b14690b8167bb523
Brian Barrett <bbarrett@amazon.com>
2018-05-08 00:35:54 +0000

    ompi: release 3.1.0
    It lives!
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>
    
    Former-commit-id: bef1bfa6a116ddbf68a406c22387f303e203506c

---
+++ doc/v3.1/index.php
@@ -0,0 +1,4 @@
+<?php
+$topdir = "../..";
+include_once("data-3.1.0.inc");
+include_once("../engine.inc");
--- index.php
+++ index.php
@@ -87,6 +87,9 @@ information</a></h3>
 
 <?php
 # Lists are not mirrored - use an absolute URL
+news("Open MPI v3.1.0 released",
+     "Bug fix release",
+     "https://www.mail-archive.com/announce@lists.open-mpi.org//msg00109.html");
 news("Open MPI v3.0.1 released",
      "Bug fix release",
      "https://www.mail-archive.com/announce@lists.open-mpi.org//msg00108.html");
