fe1c2ff6bd9032d6dcb0002e7ce6dcc8dd0eb4cd
Brice Goglin <Brice.Goglin@inria.fr>
2018-08-27 15:35:54 +0200

    hwloc 2.0.2rc2 and 1.11.11rc2
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- projects/hwloc/index.php
+++ projects/hwloc/index.php
@@ -16,12 +16,12 @@ include_once("$topdir/includes/code.inc");
 <div style="float: right; clear: both"><!-- "clear: both" prevents news from going near the float image above -->
 <?php
 # Lists are not mirrored - use an absolute URL
-news("hwloc v2.0.2rc1 released",
+news("hwloc v2.0.2rc2 released",
      "Stable release candidate",
-     "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00113.html");
-news("hwloc v1.11.11rc1 released",
+     "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00115.html");
+news("hwloc v1.11.11rc2 released",
      "Ultrastable release candidate",
-     "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00114.html");
+     "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00116.html");
 news("hwloc v2.0.1 released",
      "Stable release",
      "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00112.html");
