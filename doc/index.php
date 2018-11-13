e3e0cc02f6b1ea7af17683be43595c3afa14f053
Howard Pritchard <howardp@lanl.gov>
2018-11-13 06:59:26 -0700

    add man pages for v4.0.x
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- doc/index.php
+++ doc/index.php
@@ -12,11 +12,11 @@ Documentation for the following versions is available:
 
 <p><ul>
 
-<li><a href="current/"><strong>v3.1 series</strong></a>.
-This documentation reflects the latest progression in the 3.1.x series.
+<li><a href="current/"><strong>v4.0 series</strong></a>.
+This documentation reflects the latest progression in the 4.0.x series.
 <strong><font
 color=red><?php
-  $t = mktime(0, 0, 0, 4, 1, 2017);
+  $t = mktime(0, 0, 0, 2, 1, 2019);
   if (time() < $t) {
           printf("This stable series is relatively new.  Please upgrade with caution and test your codes thoroughly.");
   } else {
@@ -29,6 +29,12 @@ color=red><?php
 <h2>Prior stable release series</h2>
 
 <p><ul>
+
+<li><a href="current/"><strong>v3.1 series</strong></a>.
+This documentation reflects the latest progression in the 3.1.x series.
+<strong><font color=red>This code base is still supported, but is 
+in "bug fix only" mode.</font></strong>
+strong></li>
 
 <li><a href="v3.0/"><strong>v3.0 series
 </strong></a>.  This documentation reflects
@@ -46,20 +52,18 @@ new features (compared to the v2.0 series).
 in "bug fix only" mode.</font></strong>
 </li>
 
-<li><a href="v2.0/"><strong>v2.0 series
-</strong></a> (prior stable release series).  This documentation reflects
-the latest progression in the 2.0.x series.  The emphasis of this
-tree is on bug fixes and stability, although it also introduced many
-new features (compared to the v1.10 series).  <strong><font
-color=red>This code base is still supported, but is in "bug fix only"
-mode.</font></strong></li>
-
 </ul></p>
 
 <h2>Older series (retired, deprecated, or otherwise no longer in
 development)</h2>
 
 <p><ul>
+
+<li><a href="v2.0/"><strong>v2.0 series
+</strong></a> (prior stable release series).  This documentation reflects
+the latest progression in the 2.0.x series.  The emphasis of this
+tree is on bug fixes and stability, although it also introduced many
+new features (compared to the v1.10 series).</li>
 
 <li><a href="v1.10/"><strong>v1.10 series
 </strong></a> (prior stable release series).  This documentation reflects
