c42397430531020c62b24c376a69eb018ab5c7f1
Howard Pritchard <howardp@lanl.gov>
2019-03-27 09:25:29 -0600

    OMPI: updates for v4.0.1 release
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- index.php
+++ index.php
@@ -87,6 +87,9 @@ information</a></h3>
 
 <?php
 # Lists are not mirrored - use an absolute URL
+news("Open MPI v4.0.1 released",
+     "Bug fix release",
+     "https://www.mail-archive.com/announce@lists.open-mpi.org/msg00122.html");
 news("Open MPI v2.1.6 released",
      "Bug fix release",
      "https://www.mail-archive.com/announce@lists.open-mpi.org/msg00121.html");
