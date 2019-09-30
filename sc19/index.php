f69a162d257f3e5adf406856079f8b86858fe3d5
Jeff Squyres <jsquyres@cisco.com>
2019-09-30 12:43:56 -0700

    Add first version of sc19 page: Google form iframe
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

---
+++ sc19/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$topdir = "..";
+$title = "Open MPI State of the Union XIII BOF at SC'19";
+$short_title = "OMPI SotU BOF@SC'19";
+
+include_once("$topdir/includes/header.inc");
+?>
+
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd-Q-JKc95JnZrA1B7zHARVwjly46sb1kupgsTmp3HL9pUcMw/viewform?embedded=true" width="760" height="1500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
+<?php
+include_once("$topdir/includes/footer.inc");
