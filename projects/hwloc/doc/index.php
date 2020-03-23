1f58599de6499f52d407193af70a7f3d5a83294d
Brice Goglin <Brice.Goglin@inria.fr>
2020-03-23 11:26:43 +0100

    hwloc: 2.2.0rc1
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- projects/hwloc/doc/index.php
+++ projects/hwloc/doc/index.php
@@ -36,12 +36,26 @@ $blank_line = "<tr><td style=\"background-color:#999999\" colspan=\"4\"></td></t
 
 #########################################################
 
+$v = "v2.2.0rc1";
+# See if there's doc for this prerelease, display it.
+if (preg_match("/[a-z]/i", $v) &&
+    file_exists("$v") &&
+    file_exists("hwloc-$v-a4.pdf") &&
+    file_exists("hwloc-$v-letter.pdf")) {
+    print($blank_line);
+
+    print_docs("Pre-release $v (newest of the new, unstable)",
+               "hwloc-$v-letter.pdf", "hwloc-$v-a4.pdf", "$v/");
+}
+
+#########################################################
+
 print($blank_line);
 
 $versions = array("v2.1.0");
 $first = 1;
 foreach ($versions as $key => $v) {
-    print_docs("Release $v (stable" . (!$first ? ", old" : "") . ")",
+    print_docs("Release $v (stable)",
                "hwloc-$v-letter.pdf", "hwloc-$v-a4.pdf", "$v/");
     $first = 0;
 }
@@ -67,7 +81,7 @@ print($blank_line);
 $versions = array("v2.0.4", "v2.0.3", "v2.0.2", "v2.0.1", "v2.0.0");
 $first = 1;
 foreach ($versions as $key => $v) {
-    print_docs("Release $v (stable" . (!$first ? ", old" : "") . ")",
+    print_docs("Release $v (stable, old)",
                "hwloc-$v-letter.pdf", "hwloc-$v-a4.pdf", "$v/");
     $first = 0;
 }
