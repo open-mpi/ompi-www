be89cf7a75264f4a9ed25ea386b970ae042e7b70
Brian Barrett <bbarrett@amazon.com>
2020-07-07 01:24:46 +0000

    v4.1: Create initial infrastructure and post 4.1.0rc1
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

---
+++ software/ompi/v4.1/index.php
@@ -0,0 +1,49 @@
+<?php
+$topdir = "../../..";
+
+include_once("version.inc");
+$title = "Open MPI: Version $release_series";
+
+include_once("$topdir/software/ompi/nav.inc");
+include_once("$topdir/includes/header.inc");
+include_once("$topdir/includes/downloads.inc");
+
+$project = "Open MPI";
+$list_name = "announce";
+$prev_describe = "the v$release_series download page";
+
+$news_url = "https://github.com/open-mpi/ompi/raw/$release_branch/NEWS";
+
+include_once("$topdir/includes/subscribe-announce.inc");
+?>
+
+<p><hr>
+
+<h2>Changes in this release:</h2>
+
+<ul>
+<li><a href="../major-changes.php">See this page</a> if you are
+upgrading from a prior major release series of Open MPI.  It shows the
+Big Changes for which end users need to be aware.</li>
+<li><?php print("<a href=\"$news_url\">"); ?>See the NEWS file</a> for
+a more fine-grained listing of changes between each release and
+sub-release of the Open MPI v<?php print($release_series);
+?> series.</li>
+</ul>
+
+<p>See the <a href="<?php print($topdir);
+?>/software/ompi/versions/timeline.php">version timeline</a> for
+information on the chronology of Open MPI releases.</p>
+
+<p>
+<div align="center">
+
+<?php
+print_release_section("openmpi", "open-mpi-release", $s3_prefix, $download_prefix,
+                       $releases, $prereleases, $cygwin_note);
+?>
+</div>
+</p>
+
+<?php
+  include_once("$topdir/includes/footer.inc");
---
+++ software/ompi/v4.1/timeline-graph.php
@@ -0,0 +1,34 @@
+<?php
+$topdir = "../../..";
+include_once("$topdir/includes/timeline-graph.inc");
+
+$vpos = 0;
+$graph = 0;
+$data = array();
+
+# Oldest milestone date
+$first_date = "2020-05-30";
+
+# The graph needs about a year time span for the title to display properly.
+# So pick either a year beyond the first date or 90 days beyond now,
+# whichever is greater.
+
+# 3 months from today
+$ts1 = time() + (90 * 24 * 60 * 60);
+# 1 year beyond the first date (this i
+$ts2 = mktime(0, 0, 0, 6, 1, 2016);
+# 1 month before the first date:
+$series_start = new DateTime($first_date);
+$series_start->modify("-1 month");
+
+$ts = (int) max($ts1, $ts2);
+$beyond_end_date = strftime("%Y-%m-%d", $ts);
+
+//////////////////////////////////////////////////////////////////////////
+// v4.0 release series
+series("v4.1 series", $series_start->format("Y-m-d"), $beyond_end_date, $data, $vpos, $graph);
+milestone("branch from v4.0.x", "2020-05-30", $data, $vpos);
+
+// Party on
+$graph->CreateSimple($data);
+$graph->Stroke();
