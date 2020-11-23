ec98a7e8e13138ccfb15f5611ff07434a1152283
Brice Goglin <Brice.Goglin@inria.fr>
2020-11-23 17:29:13 +0100

    hwloc 2.4.0rc1
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- software/hwloc/ancient/index.php
+++ software/hwloc/ancient/index.php
@@ -13,7 +13,9 @@ include_once("$topdir/includes/downloads.inc");
  Here are the links to ancient release series archives:
 </p>
 <ul>
-<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.10/">Download v1.10 (old)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v2.1/">Download v2.1 (old)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v2.0/">Download v2.0 (older)</a></li>
+<li><a href="<?php echo $topdir; ?>/software/hwloc/v1.10/">Download v1.10 (older)</a></li>
 <li><a href="<?php echo $topdir; ?>/software/hwloc/v1.9/">Download v1.9 (ancient)</a></li>
 <li><a href="<?php echo $topdir; ?>/software/hwloc/v1.8/">Download v1.8 (Antiquity)</a></li>
 <li><a href="<?php echo $topdir; ?>/software/hwloc/v1.7/">Download v1.7 (Iron Age)</a></li>
