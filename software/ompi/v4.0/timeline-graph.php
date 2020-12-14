a56e9a349d027cc89f452e0d5e8480653f84df6c
Geoffrey Paulsen <gpaulsen@us.ibm.com>
2020-12-14 13:12:10 -0600

    Updating websight for v4.0.6rc1
    Signed-off-by: Geoffrey Paulsen <gpaulsen@us.ibm.com>

--- software/ompi/v4.0/timeline-graph.php
+++ software/ompi/v4.0/timeline-graph.php
@@ -34,6 +34,7 @@ milestone("v4.0.2", "2019-10-7", $data, $vpos);
 milestone("v4.0.3", "2020-03-04", $data, $vpos);
 milestone("v4.0.4", "2020-06-11", $data, $vpos);
 milestone("v4.0.5", "2020-08-27", $data, $vpos);
+milestone("v4.0.6rc1", "2020-12-14", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
