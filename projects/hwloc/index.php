d41438b7cbab5f593ccdc5585cdd81387f09b620
Brice Goglin <Brice.Goglin@inria.fr>
2022-03-20 14:25:01 +0100

    hwloc 2.7.1rc1
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- projects/hwloc/index.php
+++ projects/hwloc/index.php
@@ -16,6 +16,9 @@ include_once("$topdir/includes/code.inc");
 <div style="float: right; clear: both"><!-- "clear: both" prevents news from going near the float image above -->
 <?php
 # Lists are not mirrored - use an absolute URL
+news("hwloc 2.7.1rc1",
+     "Stable release candidate",
+     "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00148.html");
 news("hwloc 2.7.0",
      "Major release",
      "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00147.html");
