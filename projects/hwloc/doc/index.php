6e5547f6aa848613879e6781abf0760ee82d9704
Brice Goglin <Brice.Goglin@inria.fr>
2022-12-08 11:11:03 +0100

    hwloc: remove 2.7.0 doc, 2.7.1 is enough
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- projects/hwloc/doc/index.php
+++ projects/hwloc/doc/index.php
@@ -89,7 +89,7 @@ if (preg_match("/[a-z]/i", $v) &&
 
 print($blank_line);
 
-$versions = array("v2.7.1", "v2.7.0");
+$versions = array("v2.7.1");
 $first = 1;
 foreach ($versions as $key => $v) {
     print_docs("Release $v (old)",
