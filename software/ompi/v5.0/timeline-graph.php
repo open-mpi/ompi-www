e8262380ce3877dba2c83ef6d2ec2092cbe50f0c
Austen Lauria <awlauria@us.ibm.com>
2023-01-27 15:57:28 -0500

    OMPI v5.0.0rc10 updates.
    Signed-off-by: Austen Lauria <awlauria@us.ibm.com>

--- software/ompi/v5.0/timeline-graph.php
+++ software/ompi/v5.0/timeline-graph.php
@@ -37,6 +37,7 @@ milestone("v5.0.0rc6", "2022-04-15", $data, $vpos);
 milestone("v5.0.0rc7", "2022-05-13", $data, $vpos);
 milestone("v5.0.0rc8", "2022-09-29", $data, $vpos);
 milestone("v5.0.0rc9", "2022-10-21", $data, $vpos);
+milestone("v5.0.0rc10", "2023-02-02", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
