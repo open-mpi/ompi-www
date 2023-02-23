bfdb41644e5ae266d3610f3049c4cedf9d4e628d
Brian Barrett <bbarrett@amazon.com>
2023-02-23 16:43:15 +0000

    Post v4.1.5 release
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- index.php
+++ index.php
@@ -86,6 +86,9 @@ information</a></h3>
 <td valign=top>
 
 <?php
+news("Open MPI v4.1.5 released",
+     "Bug fix release",
+     "https://www.mail-archive.com/announce@lists.open-mpi.org/msg00155.html");
 news("Open MPI v4.1.4 released",
      "Bug fix release",
      "https://www.mail-archive.com/announce@lists.open-mpi.org/msg00152.html");
