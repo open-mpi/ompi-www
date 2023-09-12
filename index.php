67a9452834930b596a3aaf4f48d0f1bba94028b7
Brice Goglin <Brice.Goglin@inria.fr>
2023-09-12 10:24:19 +0200

    hwloc 2.9.3 final
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- index.php
+++ index.php
@@ -101,9 +101,9 @@ news("Open MPI v4.1.2 released",
 news("Open MPI v4.0.7 released",
      "Bug fix release",
      "https://www.mail-archive.com/announce@lists.open-mpi.org/msg00144.html");
-news("hwloc 2.9.2",
+news("hwloc 2.9.3",
      "Stable release",
-     "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00158.html");
+     "https://www.mail-archive.com/hwloc-announce@lists.open-mpi.org/msg00160.html");
 ?>
 </td>
 </tr>
