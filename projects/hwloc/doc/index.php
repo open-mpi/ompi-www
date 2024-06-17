01684e116876c1ff2b3dbd875a57afff36366759
Brice Goglin <Brice.Goglin@inria.fr>
2024-06-17 12:42:00 +0200

    hwloc/doc: remove 2.9.* doc before 2.9.3
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- projects/hwloc/doc/index.php
+++ projects/hwloc/doc/index.php
@@ -89,7 +89,7 @@ if (preg_match("/[a-z]/i", $v) &&
 
 print($blank_line);
 
-$versions = array("v2.9.3", "v2.9.2", "v2.9.1", "v2.9.0");
+$versions = array("v2.9.3");
 $first = 1;
 foreach ($versions as $key => $v) {
     print_docs("Release $v (stable" . (!$first ? ", old" : "") . ")",
