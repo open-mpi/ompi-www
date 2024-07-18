2e32239e3d4c67fb202a5987fa2fe5021a852009
Wenduo Wang <wenduwan@amazon.com>
2024-07-18 10:00:38 -0700

    Update website for Open MPI v5.0.4
    Signed-off-by: Wenduo Wang <wenduwan@amazon.com>

--- software/ompi/v5.0/timeline-graph.php
+++ software/ompi/v5.0/timeline-graph.php
@@ -32,7 +32,7 @@ milestone("v5.0.0", "2023-10-26", $data, $vpos);
 milestone("v5.0.1", "2023-12-20", $data, $vpos);
 milestone("v5.0.2", "2024-02-06", $data, $vpos);
 milestone("v5.0.3", "2024-04-09", $data, $vpos);
-milestone("v5.0.4rc1", "2024-07-12", $data, $vpos);
+milestone("v5.0.4", "2024-07-19", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
