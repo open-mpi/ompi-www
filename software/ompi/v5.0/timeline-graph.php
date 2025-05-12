65622624b84bc4ea15730f6ca05035af67fc438c
Tomislav Janjusic <tomislavj@nvidia.com>
2025-05-12 16:49:26 -0500

    v5.0.8rc2
    Signed-off-by: Tomislav Janjusic <tomislavj@nvidia.com>

--- software/ompi/v5.0/timeline-graph.php
+++ software/ompi/v5.0/timeline-graph.php
@@ -37,6 +37,7 @@ milestone("v5.0.5", "2024-07-23", $data, $vpos);
 milestone("v5.0.6", "2024-11-15", $data, $vpos);
 milestone("v5.0.7", "2025-02-14", $data, $vpos);
 milestone("v5.0.8rc1", "2025-05-09", $data, $vpos);
+milestone("v5.0.8rc2", "2025-05-12", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
