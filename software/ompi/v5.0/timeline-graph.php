0ac07d383e03246f415b2d06e5210b93f8f65438
Tomislav Janjusic <tomislavj@nvidia.com>
2025-10-31 18:34:07 +0200

    v5.0: update for v5.0.9 release
    Signed-off-by: Tomislav Janjusic <tomislavj@nvidia.com>

--- software/ompi/v5.0/timeline-graph.php
+++ software/ompi/v5.0/timeline-graph.php
@@ -37,8 +37,7 @@ milestone("v5.0.5", "2024-07-23", $data, $vpos);
 milestone("v5.0.6", "2024-11-15", $data, $vpos);
 milestone("v5.0.7", "2025-02-14", $data, $vpos);
 milestone("v5.0.8", "2025-06-02", $data, $vpos);
-milestone("v5.0.9rc1", "2025-09-29", $data, $vpos);
-milestone("v5.0.9rc2", "2025-10-10", $data, $vpos);
+milestone("v5.0.9", "2025-10-29", $data, $vpos);
 
 // Party on
 $graph->CreateSimple($data);
