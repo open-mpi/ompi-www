e6f89fbdedcdcc8d28b81f1b2191354902c02163
Howard Pritchard <howardp@lanl.gov>
2026-09-16 12:20:07 -0600

    v6.0.0rc1
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- software/ompi/v6.0/timeline-graph.php
+++ software/ompi/v6.0/timeline-graph.php
@@ -13,6 +13,8 @@ $beyond_end_date = strftime("%Y-%m-%d", $ts);
 //////////////////////////////////////////////////////////////////////////
 // v6.0 release series
 series("v6.0 series", "2026-01-01", $beyond_end_date, $data, $vpos, $graph);
+milestone("branch from main", "2025-10-24", $data, $vpos);
+milestone("v6.0.0rc1", "2026-09-16", $data, $vpos);
 milestone("v6.0.0 (planned)", "2026-09-30", $data, $vpos);
 
 // Party on
